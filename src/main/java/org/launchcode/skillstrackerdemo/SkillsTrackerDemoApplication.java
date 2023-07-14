d459c8f313a939398556eab0ae8ba1e2f2abd8c1
unknown <rads.veerapaneni@gmail.com>
2023-07-13 21:33:25 -0500

    Studio

---
+++ src/main/java/org/launchcode/skillstrackerdemo/SkillsTrackerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.skillstrackerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsTrackerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsTrackerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/skillstrackerdemo/SkillsTrackerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.skillstrackerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsTrackerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
